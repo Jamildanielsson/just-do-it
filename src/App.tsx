b1b41647b32453438615bb303930445d26354f0c
Jamildanielsson <114480495+Jamildanielsson@users.noreply.github.com>
2024-11-06 13:01:46 +0100

    feat: initial files

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+
+
+  return (
+    <div className="wrapper">
+      <div className="main-container">
+        <Header />
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
