1f1c2eed4cbe8ab29547363bcd58f1aded300e27
Jamildanielsson <114480495+Jamildanielsson@users.noreply.github.com>
2024-11-06 13:13:58 +0100

    feat: added header + css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import './header.css'
 
 const Header = () => {
   return (
-    <div>
+    <div className='main-header'>
+    
+    <div className='col-1'>
+        <h1>6 November</h1>
+
+        <h2>2024 Wednesday</h2>
+        
+    </div>
+    <div className='col-2'>
+    <button className='add-button'>+</button>        
+    </div>
+
     </div>
   )
 }
